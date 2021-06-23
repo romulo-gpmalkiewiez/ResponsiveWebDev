88ce0f2bdb0ac08d7d6ec49107b01cce1770cbd4
Romulo Goelzer Portolann <romulogoelzer@gmail.com>
2021-06-23 20:12:20 -0300

    Aula 05 - Desafio 01

---
+++ aula05/desafio01/server.php
@@ -0,0 +1,7 @@
+<?php
+  // String em PHP no formato JSON
+  $clientes = '[ { "nome": "Caroline", "sobrenome": "Silva" },
+  { "nome": "Cristiane", "sobrenome": "Machado" } ]';
+  // Enviando os dados para a página Web
+  echo $clientes;
+?>
